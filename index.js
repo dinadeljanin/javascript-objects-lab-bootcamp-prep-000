b754ae00288eaefbb5d0b44e6fb8248b4edd5561
D D <ddeljanin5@icloud.com>
2020-03-08 19:24:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ let recipes = {};
 
 function updateObjectWithKeyAndValue(obj, key, value) {
   let newObj = Object.assign(
-    {},
-    obj[key] = value
+    obj,
+    key = value
     );
   return newObj;
 }

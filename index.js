34360ca9da827e979ea2c949835439ff84f38585
D D <ddeljanin5@icloud.com>
2020-03-08 19:33:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
   return obj;
 }
 
-function deleteFromObjectByKey(obj, key) {
+function destructivelyDeleteFromObjectByKey(obj, key) {
   delete obj[key];
   return obj;
 }
